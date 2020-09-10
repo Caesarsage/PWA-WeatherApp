b5f21461b51ec2493122d72d8773c37941ce57d7
Destiny Erhabor <destinterhabor6@gmail.com>
2020-09-10 14:10:53 +0100

    data pre caching

--- sw.js
+++ sw.js
@@ -1,22 +1,40 @@
-const staticCacheName = 'site-static';
+const staticCacheName = 'site-static-v1';
 const assets = [
-    '/'
+  '/',
+  '/index.html',
+  '/js/app.js',
+  '/js/script.js',
+  '/css/style.css',
+  '/assets/images/WeatherPrpBG.png'
 ];
 
 //install service worker
-self.addEventListener('install', evt =>{  
-    //console.log('service worker have been install');
+self.addEventListener('install', e =>{  
+  //console.log('service worker have been install');
+  e.waitUntil(
     caches.open(staticCacheName).then(cache => {
-        cache.addAll()
+        console.log('caching shell assets');
+        cache.addAll(assets);
     })
+  )
 });         
 
 //activate
-self.addEventListener('activate', evt => {
-    // console.log('service worker has been activated');
+self.addEventListener('activate', e => {
+  // console.log('service worker has been activated');
+  // e.waitUntil(
+  //   caches.keys().then(keys =>{
+  //     console.log(keys);
+  //   })
+  // )
 });
 
 //fetch
-self.addEventListener('fetch', evt => {
-    // console.log('fetch event', evt);
+self.addEventListener('fetch', e => {
+  // console.log('fetch event', evt);
+  e.respondWith(
+    caches.match(e.request).then(cacheRes =>{
+      return cacheRes || fetch(e.request);
+    })
+  );
 });
